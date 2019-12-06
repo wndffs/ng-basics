4a048801936b4cdee431c2066fdb0a89c0aa428b
windoffs <windoffs2@gmail.com>
2019-12-06 22:24:48 +0300

    card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CardComponent} from './card/card.component';
+import { FormComponent } from './form/form.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
