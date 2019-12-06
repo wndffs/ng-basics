a6e78809c445884c221d830477659d053ee09f2f
windoffs <windoffs2@gmail.com>
2019-12-06 23:16:13 +0300

    tests

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,18 @@ import {Component} from '@angular/core'
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  
+
+  title = 'My Card Title';
+  mySampleText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.';
+
+  number = 42;
+
+  obj = {
+    name: 'Sergey',
+    info: {
+      age: 29,
+      job: 'Frontend'
+    }
+  };
+
 }
